6893656a931769ded0f411fdf0f322fd2ec72c57
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-12 10:20:55 +0700

    Task 3: Finished!

--- MyMainClass.java
+++ MyMainClass.java
@@ -1,16 +1,11 @@
 public class MyMainClass {
     public static void main(String[] args) {
         SinhVienIT IT = new SinhVienIT("Nguyen Tuan Nghia", "CNTT", 10, 10, 10);
-        SinhVienBiz Biz = new SinhVienBiz("Nguyen Van Nghia", "Biz", 10, 10, 10);
-        SinhVienPoly Poly = new SinhVienPoly("Nguyen Thi Nghia", "Poly", 10, 10, 10);
-    
+        SinhVienBiz Biz = new SinhVienBiz("Nguyen Van Nghia", "Biz", 10, 10);
+
         IT.xuat();
         System.out.println("Diem: " + IT.getDiem());
         System.out.println("Hoc luc: " + IT.getHocLuc());
-    
-        Poly.xuat();
-        System.out.println("Diem: " + Poly.getDiem());
-        System.out.println("hoc luc: " + Poly.getHocLuc());
     
         Biz.xuat();
         System.out.println("Diem: " + Biz.getDiem());
